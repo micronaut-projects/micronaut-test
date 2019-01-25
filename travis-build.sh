194094c8b87dc5de8c6685ed3982db685b7dd5b2
graemerocher <graeme.rocher@gmail.com>
2019-01-25 16:36:46 +0100

    Back to snapshot

--- travis-build.sh
+++ travis-build.sh
@@ -26,11 +26,11 @@ if [[ $EXIT_STATUS -eq 0 ]]; then
 
       echo "Publishing archives"
       ./gradlew --stop
-      # if [[ -n $TRAVIS_TAG ]]; then
-      #     ./gradlew bintrayUpload --no-daemon --stacktrace || EXIT_STATUS=$?
-      # else
-      #     ./gradlew publish --no-daemon --stacktrace || EXIT_STATUS=$?
-      # fi
+      if [[ -n $TRAVIS_TAG ]]; then
+          ./gradlew bintrayUpload --no-daemon --stacktrace || EXIT_STATUS=$?
+      else
+          ./gradlew publish --no-daemon --stacktrace || EXIT_STATUS=$?
+      fi
 
       if [[ $EXIT_STATUS -eq 0 ]]; then
        ./gradlew --console=plain --no-daemon docs  || EXIT_STATUS=$?
