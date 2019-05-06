5d86e190d23e53d240cf32e5a7cbe4ec91880029
graemerocher <graeme.rocher@gmail.com>
2019-05-06 11:20:12 +0200

    Supported shared variable injection in Spock and JUnit 5

---
+++ test-junit5/src/test/java/io/micronaut/test/junit5/InjectSharedTest.java
@@ -0,0 +1,27 @@
+package io.micronaut.test.junit5;
+
+import io.micronaut.test.annotation.MicronautTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import javax.inject.Inject;
+
+@MicronautTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class InjectSharedTest {
+
+    @Inject
+    MathService mathService;
+
+    @BeforeAll
+    void setupTest() {
+        Assertions.assertNotNull(mathService);
+    }
+
+    @Test
+    void testMathService() {
+        Assertions.assertNotNull(mathService);
+    }
+}
---
+++ test-spock/src/test/groovy/io/micronaut/test/spock/InjectSharedSpec.groovy
@@ -0,0 +1,31 @@
+package io.micronaut.test.spock
+
+import io.micronaut.test.annotation.MicronautTest
+import spock.lang.Shared
+import spock.lang.Specification
+import spock.lang.Stepwise
+
+import javax.inject.Inject
+
+@MicronautTest
+@Stepwise
+class InjectSharedSpec extends Specification {
+
+    @Shared
+    @Inject
+    MathService mathService
+
+    def setupSpec() {
+        assert mathService != null
+    }
+
+    void "test use shared 1"() {
+        expect:
+        mathService.compute(10) == 40
+    }
+
+    void "test use shared 2"() {
+        expect:
+        mathService.compute(10) == 40
+    }
+}
