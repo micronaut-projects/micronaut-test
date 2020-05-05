182c698a6cd56e64bf8883098f3afd6b48e5f227
Sergio del Amo <sergio.delamo@softamo.com>
2020-05-05 13:06:06 +0200

    feat: add test for transactional gorm support

---
+++ test-spock/src/test/groovy/io/micronaut/test/spock/entities/Book.groovy
@@ -0,0 +1,13 @@
+package io.micronaut.test.spock.entities
+
+import grails.gorm.annotation.Entity
+import org.grails.datastore.gorm.GormEntity
+
+@Entity
+class Book implements GormEntity<Book> {
+    String name
+
+    static constraints = {
+        name nullable: false, size: 1..100
+    }
+}
