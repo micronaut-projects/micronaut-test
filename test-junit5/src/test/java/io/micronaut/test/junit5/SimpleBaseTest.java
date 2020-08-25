bdb6be4445e36413a451621320e7fe03c45abac4
Alvaro Sanchez-Mariscal <alvaro.sanchezmariscal@gmail.com>
2020-08-25 18:48:22 +0200

    Add tests to reproduce #223

---
+++ test-junit5/src/test/java/io/micronaut/test/junit5/SimpleBaseTest.java
@@ -0,0 +1,14 @@
+package io.micronaut.test.junit5;
+
+import io.micronaut.test.annotation.MockBean;
+
+import static org.mockito.Mockito.mock;
+
+public abstract class SimpleBaseTest {
+
+    @MockBean(SimpleService.class)
+    SimpleService simpleService() {
+        return mock(SimpleService.class);
+    }
+
+}
---
+++ test-junit5/src/test/java/io/micronaut/test/junit5/SimpleService.java
@@ -0,0 +1,17 @@
+package io.micronaut.test.junit5;
+
+import javax.inject.Singleton;
+
+@Singleton
+public class SimpleService {
+
+    private SimpleWorker simpleWorker;
+
+    public SimpleService(SimpleWorker simpleWorker) {
+        this.simpleWorker = simpleWorker;
+    }
+
+    public SimpleWorker getSimpleWorker() {
+        return simpleWorker;
+    }
+}
