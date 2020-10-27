0d74ac99ff9c99158b3ab79d4255a320b6051d41
graemerocher <graeme.rocher@gmail.com>
2020-10-27 09:40:37 +0100

    Allow micronaut test to manage execution of external process erver

---
+++ test-junit5/src/test/java/io/micronaut/test/junit5/server/ProcessServerTest.java
@@ -0,0 +1,65 @@
+package io.micronaut.test.junit5.server;
+
+import io.micronaut.context.annotation.Property;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.HttpStatus;
+import io.micronaut.http.client.RxHttpClient;
+import io.micronaut.http.client.annotation.Client;
+import io.micronaut.runtime.server.EmbeddedServer;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import io.micronaut.test.support.TestPropertyProvider;
+import io.micronaut.test.support.server.TestEmbeddedServer;
+import io.micronaut.test.support.server.TestExecutableEmbeddedServer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import javax.inject.Inject;
+import java.util.Collections;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@MicronautTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@Property(
+    name = TestExecutableEmbeddedServer.PROPERTY,
+    value = "src/test/apps/test-app.jar"
+)
+public class ProcessServerTest implements TestPropertyProvider {
+
+    @Inject
+    EmbeddedServer embeddedServer;
+
+
+    @Inject
+    @Client("/")
+    RxHttpClient client;
+
+    @Test
+    void testServerAvailable() {
+        HttpResponse<String> response = client.exchange("/test", String.class).blockingFirst();
+
+        assertTrue(
+                embeddedServer instanceof TestExecutableEmbeddedServer
+        );
+        assertTrue(
+                embeddedServer.isRunning()
+        );
+        assertEquals(
+                HttpStatus.OK,
+                response.status()
+        );
+        assertEquals(
+                "Result = good",
+                response.body()
+        );
+    }
+
+    @Override
+    public Map<String, String> getProperties() {
+        return Collections.singletonMap(
+                "test.property", "good"
+        );
+    }
+}
