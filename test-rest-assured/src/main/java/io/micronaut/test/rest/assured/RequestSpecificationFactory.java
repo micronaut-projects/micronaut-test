61ec528761b9bb6af96329c82492255bd880b8d6
Graeme Rocher <graeme.rocher@gmail.com>
2022-06-06 10:47:31 +0200

    checkstyle

--- test-rest-assured/src/main/java/io/micronaut/test/rest/assured/RequestSpecificationFactory.java
+++ test-rest-assured/src/main/java/io/micronaut/test/rest/assured/RequestSpecificationFactory.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2017-2022 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.micronaut.test.rest.assured;
 
 import io.micronaut.context.annotation.Factory;
@@ -14,14 +29,15 @@ import io.restassured.specification.RequestSpecification;
  * @since 3.4.0 
  */
 @Factory
-@Requires(beans=EmbeddedServer.class) 
+@Requires(beans = EmbeddedServer.class) 
 public class RequestSpecificationFactory {
 
     /**
+     * @param embeddedServer The embedded server
      * @return A request specification for the current server.
      */
     @Refreshable
-    @Requires(beans=EmbeddedServer.class) 
+    @Requires(beans = EmbeddedServer.class) 
     RequestSpecification requestSpecification(EmbeddedServer embeddedServer) {
         return RestAssured.given()
             .port(embeddedServer.getPort());
