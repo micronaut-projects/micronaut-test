25301c2d8477d0d512ac668565c774d864a9f7f7
Sergio del Amo <sergio.delamo@softamo.com>
2023-10-13 13:07:05 +0200

    fix id

--- test-suite-at-sql-jpa/src/test/resources/threeproducts.sql
+++ test-suite-at-sql-jpa/src/test/resources/threeproducts.sql
@@ -1,3 +1,3 @@
 insert into products(id, code, name) values(1, 'p101', 'Apple MacBook Pro') ON CONFLICT DO NOTHING;
 insert into products(id, code, name) values(2, 'p102', 'Sony TV') ON CONFLICT DO NOTHING;
-insert into products(id, code, name) values(2, 'p103', 'Lenovo TV') ON CONFLICT DO NOTHING;
+insert into products(id, code, name) values(3, 'p103', 'Lenovo TV') ON CONFLICT DO NOTHING;
