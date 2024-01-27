5e24562a0bd3b4ac1434fad9c98c35fc37de75ea
altro3 <alexuyt@gmail.com>
2024-01-27 16:03:25 +0700

    Move junit libs to managed block.
    Fixed #831

--- test-suite-at-sql-jpa/build.gradle.kts
+++ test-suite-at-sql-jpa/build.gradle.kts
@@ -8,22 +8,23 @@ repositories {
 
 dependencies {
     annotationProcessor(mn.micronaut.inject.java)
-    testAnnotationProcessor(mn.micronaut.inject.java)
-
     annotationProcessor(mnData.micronaut.data.processor)
+
     implementation(mnData.micronaut.data.hibernate.jpa)
     implementation(mnSql.micronaut.jdbc.hikari)
-    runtimeOnly(mnSql.postgresql)
 
+    runtimeOnly(mnSql.postgresql)
     runtimeOnly(mnLogging.logback.classic)
 
-    testImplementation(libs.junit.jupiter.api)
-    testImplementation(projects.micronautTestJunit5)
-    testRuntimeOnly(libs.junit.jupiter.engine)
+    testAnnotationProcessor(mn.micronaut.inject.java)
 
     testImplementation(platform(libs.testcontainers.bom))
+    testImplementation(libs.managed.junit.jupiter.api)
+    testImplementation(projects.micronautTestJunit5)
     testImplementation(libs.testcontainers)
     testImplementation(libs.testcontainers.postgresql)
+
+    testRuntimeOnly(libs.managed.junit.jupiter.engine)
 }
 tasks.withType<Test> {
     useJUnitPlatform()
