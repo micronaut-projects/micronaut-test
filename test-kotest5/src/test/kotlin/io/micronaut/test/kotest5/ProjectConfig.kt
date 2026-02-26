15234b66309abbbc739c49cb8004033e51cdb667
issam liqali <liqali.issam@oracle.com>
2026-02-26 12:33:13 +0000

    update to kotest 6

--- test-kotest5/src/test/kotlin/io/micronaut/test/kotest5/ProjectConfig.kt
+++ test-kotest5/src/test/kotlin/io/micronaut/test/kotest5/ProjectConfig.kt
@@ -6,5 +6,5 @@ import io.micronaut.test.extensions.kotest5.MicronautKotest5Extension
 
 @Suppress("unused")
 object ProjectConfig : AbstractProjectConfig() {
-    override fun extensions() = listOf(MicronautKotest5Extension)
+    override val extensions = listOf(MicronautKotest5Extension)
 }
